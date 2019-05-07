ff0e4e5085746111a48d905caaafeff2d2c9b6e1
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2019-05-07 18:52:15 +0200

    zadanie3 tests

--- zadanie3.py
+++ zadanie3.py
@@ -2,19 +2,23 @@ import unittest
 
 
 def extract_elements(list1, list2):
-    for i in range(len(list2)):
-        if isinstance(list2[i], int):
-            pass
-        else:
-            return "wrong input"
-    try:
-        return [list1[x] for x in list2]
-    except IndexError:
-        return 'index out of range'
+    if type(list1) is list and type(list2) is list:
+        for i in range(len(list2)):
+            if isinstance(list2[i], int):
+                pass
+            else:
+                return "wrong input"
+        try:
+            return [list1[x] for x in list2]
+        except IndexError:
+            return 'index out of range'
+    else:
+        return "wrong input"
+
 
 lista1 = [5, 10, 15, 20]
 lista2 = [3, 1, 2]
-print(extract_elements(lista1, lista2))
+extract_elements(lista1, lista2)
 
 
 class TestExtractElements(unittest.TestCase):
@@ -24,15 +28,38 @@ class TestExtractElements(unittest.TestCase):
     def test_negative_numb_list2(self):
         self.assertEqual(extract_elements(lista1, [-1]), [20])
 
+    def test_empty_list1(self):
+        self.assertEqual(extract_elements([], lista2), 'index out of range')
+
     def test_empty_list2(self):
         self.assertEqual(extract_elements(lista1, []), [])
 
-    def test_float_list2(self):
+    def test_empty_both(self):
+        self.assertEqual(extract_elements([], []), [])
+
+    def test_float_in_list2(self):
         self.assertEqual(extract_elements(lista1, [2.0]), 'wrong input')
 
-    def test_string_list2(self):
+    def test_string_in_list2(self):
         self.assertEqual(extract_elements(lista1, ['str']), 'wrong input')
 
     def test_out_of_range_list2(self):
         self.assertEqual(extract_elements(lista1, [5]), 'index out of range')
 
+    def test_float_instead_list2(self):
+        self.assertEqual(extract_elements(lista1, 2.0), 'wrong input')
+
+    def test_float_instead_list1(self):
+        self.assertEqual(extract_elements(2.0, [0]), 'wrong input')
+
+    def test_str_instead_list2(self):
+        self.assertEqual(extract_elements(lista1, 'str'), 'wrong input')
+
+    def test_str_instead_list1(self):
+        self.assertEqual(extract_elements('str', [0]), 'wrong input')
+
+    def test_int_instead_list2(self):
+        self.assertEqual(extract_elements(lista1, 0), 'wrong input')
+
+    def test_int_instead_list1(self):
+        self.assertEqual(extract_elements(0, [0]), 'wrong input')
